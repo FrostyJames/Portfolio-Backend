f27aba758c1be8cdc651c1e883707cc501ca09bc
James Ivan <ivanlavan773@gmail.com>
2026-09-08 09:01:25 +0300

    Add email test

---
+++ resources/views/emails/test.blade.php
@@ -0,0 +1,5 @@
+<h1>Portfolio Email Test</h1>
+
+<p>This is a test email from your Laravel portfolio backend.</p>
+
+<p>If you received this message, Laravel and Brevo SMTP are working correctly.</p>
--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,15 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProjectController;
 use App\Http\Controllers\ContactController;
-
+use App\Mail\TestEmail;
+use Illuminate\Support\Facades\Mail;
 
 Route::post('/contact', [ContactController::class, 'store']);
 Route::get('/projects', [ProjectController::class, 'index']);
+
+//endpoint for testing email sending
+Route::get('/test-email', function () {
+    Mail::to('ivanlavan773@gmail.com')->send(new TestEmail());
+
+    return 'Test email sent.';
+});3
